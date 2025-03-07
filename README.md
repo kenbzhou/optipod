dff6782cdb671ba52d87938d241650d626d63605
Ken Zhou <zhoubken@gmail.com>
2025-03-07 23:12:06 +0000

    Structured ebpf-profiler into a class in anticipation of flask mulithtreading; updated README.md and requirements

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 # eBPF-Scheduler
 
-## Installation Instructions for eBPF Profiler
-To install the eBPF profiler, SSH into your Linux instance and run the following commands:
+## Installation Instructions
+### eBPF Profiler
+To install requirements for the eBPF profiler, SSH into your Linux instance and run the following commands:
 ```
 sudo apt-get install linux-tools-common linux-tools-generic linux-tools-$(uname -r)
 
 sudo apt-get install -y bpfcc-tools linux-headers-$(uname -r) python3-bpfcc build-essential linux-tools-common linux-tools-generic python3-pip git
 ```
+From the repository directory, also run `sudo pip3 install -r requirements.txt` to install the Python requirements.
 
+### Local Prometheus Server (for Testing)
+To install Prometheus, run these following commands from within the repository:
+```
+curl -LO https://github.com/prometheus/prometheus/releases/download/v3.2.1/prometheus-3.2.1.linux-amd64.tar.gz
+
+tar -xvf prometheus-*.tar.gz
+```
+
+Copy the contents of the file `prometheus.yml` to the file of the same name within the `prometheus` folder
+
+
+## Running the Profiler
 To run the eBPF profiler, run `sudo python3 ebpf-profiler.py`.
+
--- ebpf-profiler/ebpf-profiler.py
+++ ebpf-profiler/ebpf-profiler.py
@@ -1,25 +1,60 @@
 #!/usr/bin/python3
-from bcc import BPF
 import time
+from bcc import BPF
 from datetime import datetime
 from profiler_string import profiler_program
 from ctypes import Structure, c_ulonglong, c_ulong
- 
-ENV_VAR: str = "ENV_TEST" # Replace this
 
-# Initialize BPF
-b = BPF(text=profiler_program)
+LOG_IN_TERMINAL: bool = True
+NODE_ID: str = "TEST_ID_1" # Replace this
 
-# Attach kprobes
-b.attach_kprobe(event="finish_task_switch.isra.0", fn_name="trace_ctx_switches")
-b.attach_kprobe(event="handle_mm_fault", fn_name="trace_page_faults")
-b.attach_kprobe(event="__kmalloc", fn_name="trace_memory_allocation")
-b.attach_kprobe(event="vfs_read", fn_name="trace_fs_read")
-b.attach_kprobe(event="vfs_write", fn_name="trace_fs_write")
+# Profiler code for EBPF
+# measured metrics: page faults, mem allocations, context switches (graceful, forced), filesystem read/write cts and sizes.
+class EBPF_Profiler:
+   def __init__(self):
+      # Initialize BPF
+      self.profiler = BPF(text=profiler_program)
 
-# b.attach_kprobe(event="kfree", fn_name="trace_memory_deallocation")
+      # Attach kprobes
+      self.profiler.attach_kprobe(event="finish_task_switch.isra.0", fn_name="trace_ctx_switches")
+      self.profiler.attach_kprobe(event="handle_mm_fault", fn_name="trace_page_faults")
+      self.profiler.attach_kprobe(event="__kmalloc", fn_name="trace_memory_allocation")
+      self.profiler.attach_kprobe(event="vfs_read", fn_name="trace_fs_read")
+      self.profiler.attach_kprobe(event="vfs_write", fn_name="trace_fs_write")
 
-print(f"Starting profiler...")
+      print(f"Starting profiler...")
+   
+   def decode_timestamp(self, timestamp: c_ulong):
+      timestamp_ns = timestamp.value * 10000000000 # 10s
+      timestamp_sec = timestamp_ns // 1000000000   # 10s
+      return datetime.utcfromtimestamp(timestamp_sec).strftime('%H:%M:%S')
+
+   def print_logging_header(self):
+      if LOG_IN_TERMINAL:
+         print("\n" + "=" * 100)
+         print(f"Profiling start: {datetime.now().strftime('%H:%M:%S')}")
+         print("=" * 100)
+         print("%-9s %-10s %-10s %-10s %-10s %-10s %-16s %-10s %-16s" % ("TIMESTAMP", "CTX_SW_G", "CTX_SW_F", "MEM_ALLOC", "PAGE_FTS", "FS_READ_CT", "FS_READ_KB", "FS_WRT_CT", "FS_WRT_KB"))
+
+   def print_last_metric_log(self, timestamp, metrics):
+      if LOG_IN_TERMINAL:
+         print("%-9s %-10s %-10s %-10s %-10s %-10s %-16s %-10s %-16s" % (self.decode_timestamp(timestamp), metrics.ctx_switches_graceful, metrics.ctx_switches_forced, metrics.mem_bytes_allocated, metrics.page_faults, 
+                                                                        metrics.fs_read_count, metrics.fs_read_size_kb, metrics.fs_write_count, metrics.fs_write_size_kb))
+   
+   def profiler_loop(self):
+      self.print_logging_header()
+      # Schema: since there's bleed, we're guaranteed that the second to last entry is completed.
+      # Print out that 'earliest entry', then delete it from the table to save space.
+      # Let Prometheus handle data storage/consistency.
+      while True:
+         time.sleep(10)
+         # Process CPU metrics
+         timestamped_profile = self.profiler.get_table("timestamped_profile")
+         sorted_entries = sorted([(key, value) for key, value in timestamped_profile.items()], key=lambda x: datetime.strptime(self.decode_timestamp(x[0]), '%H:%M:%S'))
+         earliest, val_earliest = sorted_entries[0]
+         metrics = ProfiledMetrics.from_buffer_copy(val_earliest)
+         self.print_last_metric_log(earliest, metrics)
+         del timestamped_profile[earliest]
 
 class ProfiledMetrics(Structure):
    _fields_ = [
@@ -36,30 +71,6 @@ class ProfiledMetrics(Structure):
       ("fs_write_size_kb",        c_ulonglong),
     ]
 
-def decode_timestamp(timestamp: c_ulong):
-   timestamp_ns = timestamp.value * 10000000000 # 10s
-   timestamp_sec = timestamp_ns // 1000000000   # 10s
-   return datetime.utcfromtimestamp(timestamp_sec).strftime('%H:%M:%S')
-
-try:
-   print("\n" + "=" * 100)
-   print(f"Profiling start: {datetime.now().strftime('%H:%M:%S')}")
-   print("=" * 100)
-   print("%-9s %-10s %-10s %-10s %-10s %-10s %-16s %-10s %-16s" % ("TIMESTAMP", "CTX_SW_G", "CTX_SW_F", "MEM_ALLOC", "PAGE_FTS", "FS_READ_CT", "FS_READ_KB", "FS_WRT_CT", "FS_WRT_KB"))
-   # Schema: since there's bleed, we're guaranteed that the second to last entry is completed.
-   # Print out that 'earliest entry', then delete it from the table to save space.
-   # Let Prometheus handle data storage/consistency.
-   while True:
-      time.sleep(10)
-      # Process CPU metrics
-      timestamped_profile = b.get_table("timestamped_profile")
-      sorted_entries = sorted([(key, value) for key, value in timestamped_profile.items()], key=lambda x: datetime.strptime(decode_timestamp(x[0]), '%H:%M:%S'))
-      earliest, val_earliest = sorted_entries[0]
-      metrics = ProfiledMetrics.from_buffer_copy(val_earliest)
-      print("%-9s %-10s %-10s %-10s %-10s %-10s %-16s %-10s %-16s" % (decode_timestamp(earliest), metrics.ctx_switches_graceful, metrics.ctx_switches_forced, metrics.mem_bytes_allocated, metrics.page_faults, 
-                                                                     metrics.fs_read_count, metrics.fs_read_size_kb, metrics.fs_write_count, metrics.fs_write_size_kb))
-      del timestamped_profile[earliest]
-
-
-except:
-   print("Some fault occurred")
+if __name__ == '__main__':
+   profiler = EBPF_Profiler()
+   profiler.profiler_loop()
